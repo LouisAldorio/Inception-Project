b6caf16a1e88dfb7634e015b1398137c11c00cbf
louisaldorio <louisaldorio@gmail.com>
2020-10-24 17:20:05 +0700

    minor

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,7 +5,7 @@ import {IonItem,IonLabel,IonInput,IonSelect,IonSelectOption} from '@ionic/react'
 function InputControls(props){
     return(
         <IonItem>
-            <IonLabel position="floating">{props.display}</IonLabel>
+            <IonLabel position="floating" color="medium">{props.display}</IonLabel>
             <IonInput type={props.type} ref={props.ref} name={props.name} autofocus={props.focus} clearInput="true" onIonChange={props.onChange}></IonInput>
         </IonItem>
     )
