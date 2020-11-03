9e2422a89032f8cb0ad8ed1ca4f9f15d11d3090e
felixyangsen <felixyangsen96@gmail.com>
2020-11-03 11:12:47 +0700

    add more functionality

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,31 +1,55 @@
 import React,{useContext} from 'react'
 import Moment from 'moment'
-import {IonCard,IonCardHeader,IonCardContent,IonCardSubtitle,IonCardTitle} from '@ionic/react'
+import {IonCard,IonCardHeader,IonCardContent,IonCardSubtitle,IonCardTitle, IonButton, IonLabel,IonIcon,IonModal,IonHeader,IonToolbar,IonTitle,IonButtons} from '@ionic/react'
+import {chatbubbleOutline} from 'ionicons/icons'
 
 import LikePost from './LikePost'
 import {AuthContext} from '../context/Auth'
+import { useState } from 'react'
+
+import SinglePost from '../pages/singlePost'
 
 function PostCard({ post: { id, body, createdAt, username, likesCount, commentsCount, likes } }){
     const {user} = useContext(AuthContext)
 
+    const [modalState,setModalState] = useState(false)
 
-    return (
 
-        
-        <IonCard routerAnimation>
-            <IonCardContent href={`/posts/${id}`} >
-                <IonCardHeader>
-                    <IonCardSubtitle>{Moment(createdAt).fromNow()}</IonCardSubtitle>
-                    <IonCardTitle>{username}</IonCardTitle>
-                </IonCardHeader>
-                <IonCardContent>
-                    {body}
+    return (  
+        <React.Fragment>  
+            <IonCard routerAnimation>
+                <IonCardContent onClick={() => setModalState(true)} button={true}>
+                    <IonCardHeader>
+                        <IonCardSubtitle>{Moment(createdAt).fromNow()}</IonCardSubtitle>
+                        <IonCardTitle>{username}</IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                        {body}
+                    </IonCardContent>
                 </IonCardContent>
-            </IonCardContent>
-            <IonCardContent>
-                <LikePost user={user} post={{id,likes,likesCount}}/>
-            </IonCardContent> 
-        </IonCard>
+
+                <IonCardContent>
+                    <LikePost user={user} post={{id,likes,likesCount}}/>
+                    <IonButton onClick={() => setModalState(true)} button={true}>
+                        <IonIcon slot="start" icon={chatbubbleOutline} />
+                        <IonLabel basic color='blue' pointing='left'>
+                            {commentsCount}
+                        </IonLabel>
+                    </IonButton>
+                </IonCardContent> 
+            </IonCard>
+            <IonModal isOpen={modalState}>
+                <IonHeader translucent>
+                    <IonToolbar color='dark'>
+                        <IonTitle>Post Detail!</IonTitle>
+                        <IonButtons slot="end">
+                            <IonButton onIonFocus={()=> setModalState(false)}>Close</IonButton>
+                        </IonButtons>
+                    </IonToolbar>
+                </IonHeader>
+                <SinglePost postID={id}/>
+            </IonModal>
+        </React.Fragment> 
     )
 }
 
