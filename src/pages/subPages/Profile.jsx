0cbe6512c7809788dd7a44bb08d183917a978550
susisyahrir <susisyahrir@gmail.com>
2020-11-28 08:01:43 +0700

    minor

--- src/pages/subPages/Profile.jsx
+++ src/pages/subPages/Profile.jsx
@@ -17,20 +17,47 @@ function Profile(props){
 
     let { photos,takePhoto,setPhotos } = usePhotoGallery();
 
-    if(photos) {
-        
+    if(photos) {      
         updateProfilePic(photos)
     }
 
+    const [uploadedImageData,setUploadedImagedata] = useState({})
+
+    
+
     function updateProfilePic(photo){
         setProfilePicture(photo)
-        //jalankan mutasi update profile user dan upload foto ke cloud
 
+        var formData = new FormData()
+        formData.append('File',photo)
+        formData.append('Type_Adaptor','GDRIVE')
+        for (var key of formData.entries()) {
+            console.log(key[0] + ', ' + key[1]);
+        }
+        //jalankan mutasi update profile user dan upload foto ke cloud
+        fetch("http://localhost:8087/upload" , {
+            // content-type header should not be specified!
+            mode: 'no-cors',
+            method: 'POST',
+            body: formData,
+        }).then(response => {
+            console.log(response.text())
+          })
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+
+        
 
         //set photos jadi kosong lg untuk menghindari re render yang cukup banyak
         setPhotos()
+        // console.log(uploadedImageData)
     }
-
+    
+    console.log(uploadedImageData)
 
     return (
         <React.Fragment>
