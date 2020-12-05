4bbde735c1c4bf77739a2ccca2b7979a20d3cf7d
louisaldorio <louisaldorio@gmail.com>
2020-12-05 16:09:10 +0700

    temp

--- src/pages/subPages/CommodityPost.jsx
+++ src/pages/subPages/CommodityPost.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import {useQuery} from '@apollo/client'
-import { IonCard, IonCardContent,IonLabel,IonToast, IonContent,IonFabButton,IonIcon,IonBackButton, IonCardHeader,IonModal,IonHeader,IonToolbar,IonTitle,IonButtons,IonFab,IonText,IonSlide,IonSlides, IonInfiniteScroll, IonInfiniteScrollContent, IonSpinner, IonItem, IonAvatar, IonRow, IonCol, IonCardTitle, IonChip } from '@ionic/react'
+import { useQuery } from '@apollo/client'
+import { IonCard, IonCardContent, IonLabel, IonToast, IonContent, IonFabButton, IonIcon, IonBackButton, IonCardHeader, IonModal, IonHeader, IonToolbar, IonTitle, IonButtons, IonFab, IonText, IonSlide, IonSlides, IonInfiniteScroll, IonInfiniteScrollContent, IonSpinner, IonItem, IonAvatar, IonRow, IonCol, IonCardTitle, IonChip } from '@ionic/react'
 import '../../App.css'
 import { useContext } from 'react'
 import { AuthContext } from '../../context/Auth'
 import ImageZoom from '../../components/PhotoZoom'
-import {add,pricetag,cart,mail,call,personAdd,checkmarkCircle} from 'ionicons/icons'
+import { add, pricetag, cart, mail, call, personAdd, checkmarkCircle } from 'ionicons/icons'
 
 import Carousel from 'react-material-ui-carousel'
 import Header from '../../components/Header'
@@ -13,55 +13,92 @@ import SearchBar from '../../components/SearchBar'
 import '@open-wa/whatsapp-button/whatsapp-button.js';
 import CommodityCreate from '../../components/CreateCommodity'
 
-function Posts(props){
+function Posts(props) {
 
-    const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
 
-    const [modalState,setModalState] = useState(false)
-    const [openCreateForm,setOpenCreateForm] = useState(false)
-    const [searchedItem,setSearchItem] = useState('')
-    const [toast,setToast] = useState(false)
+    const [modalState, setModalState] = useState(false)
+    const [openCreateForm, setOpenCreateForm] = useState(false)
+    const [searchedItem, setSearchItem] = useState('')
+    const [toast, setToast] = useState(false)
 
     const items = [
         {
-            src:["https://drive.google.com/uc?export=view&id=1Nd6n86C8jZRsII8wJ2CATVxrFtxBgLTN","https://drive.google.com/uc?export=view&id=1KB3r9uHLQ9m6VS8r177Dyac1e44I6p6K"],
-            commodityName:"Beras Raskin",
+            src: ["https://drive.google.com/uc?export=view&id=1Nd6n86C8jZRsII8wJ2CATVxrFtxBgLTN", "https://drive.google.com/uc?export=view&id=1KB3r9uHLQ9m6VS8r177Dyac1e44I6p6K"],
+            commodityName: "Beras Raskin",
             minPurchase: "100 kg",
-            unitPrice: "100.000", 
-            ComodityDescription: "The most popular industrial group ever, and largely responsible for bringing the music to a mass audience." , 
-            user:{
+            unitPrice: "100.000",
+            ComodityDescription: "The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.",
+            user: {
                 userImg: "https://drive.google.com/uc?export=view&id=1Bg1c5HJcIB2CKT17uJ53CWZpNhkYynlV",
                 username: "Louis Aldorio",
-                email:"louisaldorio@gmail.com",
-                whatsapp_number:"082161723455",
+                email: "louisaldorio@gmail.com",
+                whatsapp_number: "082161723455",
                 friend_list: ["louisaldorio"],
             }
         },
         {
-            src:["https://drive.google.com/uc?export=view&id=1Nd6n86C8jZRsII8wJ2CATVxrFtxBgLTN","https://drive.google.com/uc?export=view&id=1KB3r9uHLQ9m6VS8r177Dyac1e44I6p6K"],
-            commodityName:"Beras Merah",
+            src: ["https://drive.google.com/uc?export=view&id=1Nd6n86C8jZRsII8wJ2CATVxrFtxBgLTN", "https://drive.google.com/uc?export=view&id=1KB3r9uHLQ9m6VS8r177Dyac1e44I6p6K"],
+            commodityName: "Beras Merah",
             minPurchase: "100 kg",
-            unitPrice: "100.000", 
-            ComodityDescription: "The most popular industrial group ever, and largely responsible for bringing the music to a mass audience." ,
-            user:{
+            unitPrice: "100.000",
+            ComodityDescription: "The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.",
+            user: {
                 userImg: "https://drive.google.com/uc?export=view&id=1Bg1c5HJcIB2CKT17uJ53CWZpNhkYynlV",
                 username: "Louis Aldorio",
-                email:"louisaldorio@gmail.com",
-                whatsapp_number:"082161723455",
+                email: "louisaldorio@gmail.com",
+                whatsapp_number: "081265869573",
                 friend_list: [],
             }
         },
-        
+
     ];
 
-    const [modalData,setModalData] = useState(null)
+    const [modalData, setModalData] = useState(null)
+
+    function TestWa() {
+
+        var details = {
+            'to_number': '6282161723455',
+            'message': 'selamat anda mendapat nasi goreng sapi',
+        };
+
+        var formBody = [];
+        for (var property in details) {
+            var encodedKey = encodeURIComponent(property);
+            var encodedValue = encodeURIComponent(details[property]);
+            formBody.push(encodedKey + "=" + encodedValue);
+        }
+        formBody = formBody.join("&");
+        //jalankan mutasi update profile user dan upload foto ke cloud
+        fetch("localhost:8080/send-message", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            body: formBody,
+        }).then(response => {
+
+            var imageData = response.json()
+            imageData.then(data => {
+                // setProfilePicture(data.ImageURL)   
+                //lakukan hit mutaion ke update profile user
+
+            })
+            // setUploadProgress(false)
+
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
 
     function ToggleModal(image) {
+        TestWa()
         setModalData(image)
         setModalState(true)
     }
 
-    function CleanData(){
+    function CleanData() {
         setModalState(false)
         setModalData(null)
     }
@@ -69,87 +106,87 @@ function Posts(props){
         setSearchItem(value)
     }
 
-    function AddOrRemoveFriend(){
+    function AddOrRemoveFriend() {
         setToast(true)
     }
-    
+
     return (
-        <React.Fragment> 
-            <Header />          
+        <React.Fragment>
+            <Header />
             <IonContent scrollEvents={true}
-                    onIonScrollStart={() => {}}
-                    onIonScroll={() => {}}
-                    onIonScrollEnd={() => {}}
-                    className="ion-content-bottom">  
-                    <SearchBar selectedValue={searchedItem} changeHandler={TypeSearchWord}/>            
-                
-                    <div className="commodity-list">
-                        {items.length > 0 ? items.map((image, i) => (
-                            <IonCard className="Ubuntu" key={i} onClick={() => ToggleModal(image)} >
-                                
-                                <IonItem lines={"none"}>                             
-                                    <IonAvatar slot="start">
-                                        <img src={image.user.userImg} />
-                                    </IonAvatar>                               
-                                    <h2>{image.user.username}</h2>                               
-                                </IonItem>
-                                
-                                <img src={image.src[0]} height="250px" width="500px"/>  
-                                <IonCardContent>                             
-                                    <h1>{image.commodityName}</h1>                              
-                                </IonCardContent>   
+                onIonScrollStart={() => { }}
+                onIonScroll={() => { }}
+                onIonScrollEnd={() => { }}
+                className="ion-content-bottom">
+                <SearchBar selectedValue={searchedItem} changeHandler={TypeSearchWord} />
 
-                                <IonRow>
-                                    <IonCol size="6">
-                                        <IonItem lines={"none"}>
-                                            <IonIcon icon={pricetag} slot="start" className="pricetag"></IonIcon>
-                                            <p>Rp. {image.unitPrice}</p>
-                                        </IonItem>                                  
-                                    </IonCol>    
-                                    <IonCol size="6">
-                                        <IonItem lines={"none"}>
-                                            <IonIcon icon={cart} slot="start" className="min-purchase"></IonIcon>
-                                            <p>Min: {image.minPurchase}</p>
-                                        </IonItem>                                  
-                                    </IonCol>  
-                                </IonRow>                           
-                            </IonCard>
-                        )): <img id="photo-fullscreen" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_Tl87UxYtH39b-RUyxSB2SrtNkPZB_55dtw&usqp=CAU"/> } 
-                     </div>
+                <div className="commodity-list">
+                    {items.length > 0 ? items.map((image, i) => (
+                        <IonCard className="Ubuntu" key={i} onClick={() => ToggleModal(image)} >
 
-                    {items.length > 0 && (
-                        <IonInfiniteScroll threshold="100px" id="infinite-scroll">                    
-                            <IonInfiniteScrollContent loadingSpinner="dots"><IonSpinner color="warning" className="pagination-spinner"></IonSpinner></IonInfiniteScrollContent>
-                        </IonInfiniteScroll>  
-                    )}
-                
+                            <IonItem lines={"none"}>
+                                <IonAvatar slot="start">
+                                    <img src={image.user.userImg} />
+                                </IonAvatar>
+                                <h2>{image.user.username}</h2>
+                            </IonItem>
+
+                            <img src={image.src[0]} height="250px" width="500px" />
+                            <IonCardContent>
+                                <h1>{image.commodityName}</h1>
+                            </IonCardContent>
+
+                            <IonRow>
+                                <IonCol size="6">
+                                    <IonItem lines={"none"}>
+                                        <IonIcon icon={pricetag} slot="start" className="pricetag"></IonIcon>
+                                        <p>Rp. {image.unitPrice}</p>
+                                    </IonItem>
+                                </IonCol>
+                                <IonCol size="6">
+                                    <IonItem lines={"none"}>
+                                        <IonIcon icon={cart} slot="start" className="min-purchase"></IonIcon>
+                                        <p>Min: {image.minPurchase}</p>
+                                    </IonItem>
+                                </IonCol>
+                            </IonRow>
+                        </IonCard>
+                    )) : <img id="photo-fullscreen" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_Tl87UxYtH39b-RUyxSB2SrtNkPZB_55dtw&usqp=CAU" />}
+                </div>
+
+                {items.length > 0 && (
+                    <IonInfiniteScroll threshold="100px" id="infinite-scroll">
+                        <IonInfiniteScrollContent loadingSpinner="dots"><IonSpinner color="warning" className="pagination-spinner"></IonSpinner></IonInfiniteScrollContent>
+                    </IonInfiniteScroll>
+                )}
+
                 {user.Role === 'Supplier' && <IonFab vertical="bottom" horizontal="end" edge id="schedule-add" slot='fixed'>
                     <IonFabButton color="warning" onClick={() => setOpenCreateForm(true)}>
                         <IonIcon icon={add} />
                     </IonFabButton>
                 </IonFab>}
-                                                 
-            </IonContent>   
 
-            <CommodityCreate isOpen={openCreateForm} stateHandler={setOpenCreateForm}/>
+            </IonContent>
+
+            <CommodityCreate isOpen={openCreateForm} stateHandler={setOpenCreateForm} />
 
             <IonModal isOpen={modalState}>
                 <IonHeader translucent>
-                    <IonToolbar color='warning'>                      
-                        <IonButtons slot="start">                           
-                            <IonBackButton defaultHref="/Posts" onClick={()=> CleanData()} />                           
+                    <IonToolbar color='warning'>
+                        <IonButtons slot="start">
+                            <IonBackButton defaultHref="/Posts" onClick={() => CleanData()} />
                         </IonButtons>
                         <IonTitle>Commodity's Detail</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                           
+
                 <IonContent scrollEvents={true}>
                     <IonCard>
                         <Carousel animation="slide" autoPlay={true} interval={4000}>
                             {modalData && modalData.src.map((img) => (
-                                <ImageZoom src={img} key={img} width="500px" height="300px"/>            
+                                <ImageZoom src={img} key={img} width="500px" height="300px" />
                             ))}
-                        </Carousel>                   
+                        </Carousel>
                         <IonCardContent>
                             <IonCardTitle>
                                 <h1>Commodity: {modalData && modalData.commodityName}</h1>
@@ -157,40 +194,40 @@ function Posts(props){
                             <p>
                                 {modalData && modalData.ComodityDescription}
                             </p>
-                        </IonCardContent> 
+                        </IonCardContent>
                         <IonRow>
                             <IonCol size="6">
                                 <IonItem lines={"none"}>
                                     <IonIcon icon={pricetag} slot="start" className="pricetag"></IonIcon>
                                     <p>Rp. {modalData && modalData.unitPrice}</p>
-                                </IonItem>                                  
-                            </IonCol>    
+                                </IonItem>
+                            </IonCol>
                             <IonCol size="6">
                                 <IonItem lines={"none"}>
                                     <IonIcon icon={cart} slot="start" className="min-purchase"></IonIcon>
                                     <p>Min: {modalData && modalData.minPurchase}</p>
-                                </IonItem>                                  
-                            </IonCol>  
-                        </IonRow> 
+                                </IonItem>
+                            </IonCol>
+                        </IonRow>
                         <IonCardContent>
-                            <IonItem lines={"none"}>                             
+                            <IonItem lines={"none"}>
                                 <IonAvatar slot="start">
                                     <img src={modalData && modalData.user.userImg} />
-                                </IonAvatar>                               
-                                <h1>{modalData && modalData.user.username}</h1>    
-                                <IonChip slot="end" color="warning" onClick={AddOrRemoveFriend}><IonIcon icon={modalData && (modalData.user.friend_list.includes(user.Username) ? checkmarkCircle : personAdd)}  color="dark"></IonIcon></IonChip>                           
+                                </IonAvatar>
+                                <h1>{modalData && modalData.user.username}</h1>
+                                <IonChip slot="end" color="warning" onClick={AddOrRemoveFriend}><IonIcon icon={modalData && (modalData.user.friend_list.includes(user.Username) ? checkmarkCircle : personAdd)} color="dark"></IonIcon></IonChip>
                             </IonItem>
-                            <IonItem lines={"none"} >                             
-                                <IonIcon slot="start" icon={mail}></IonIcon>{modalData && modalData.user.email}                           
-                            </IonItem>
-                            <IonItem lines={"none"}>       
-                                <IonIcon slot="start" icon={call}></IonIcon>                                                                                       
-                                {modalData && modalData.user.whatsapp_number}                        
+                            <IonItem lines={"none"} >
+                                <IonIcon slot="start" icon={mail}></IonIcon>{modalData && modalData.user.email}
                             </IonItem>
                             <IonItem lines={"none"}>
-                                <whatsapp-button phone={modalData && modalData.user.whatsapp_number} dialcode="62" text="hey there lets chat!" label="Chat"></whatsapp-button> 
+                                <IonIcon slot="start" icon={call}></IonIcon>
+                                {modalData && modalData.user.whatsapp_number}
                             </IonItem>
-                        </IonCardContent>                     
+                            <IonItem lines={"none"}>
+                                <whatsapp-button phone={modalData && modalData.user.whatsapp_number} dialcode="62" text="hey there lets chat!" label="Chat"></whatsapp-button>
+                            </IonItem>
+                        </IonCardContent>
                     </IonCard>
                 </IonContent>
 
@@ -203,7 +240,7 @@ function Posts(props){
                 duration={800}
             />
         </React.Fragment>
-         
+
     )
 }
 
