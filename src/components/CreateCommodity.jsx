c367c7a7ec097da6b4c9eb5620b45fa01d401eb6
louisaldorio <louisaldorio@gmail.com>
2020-12-06 10:18:23 +0700

    more

--- src/components/CreateCommodity.jsx
+++ src/components/CreateCommodity.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
-import { IonModal, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonContent, IonItemGroup, IonFabButton, IonFab, IonSpinner, IonIcon, IonItem, IonAvatar } from '@ionic/react'
+import { IonModal, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonContent, IonItemGroup, IonFabButton, IonFab, IonSpinner, IonIcon, IonItem, IonAvatar,IonButton } from '@ionic/react'
 import ImageZoom from './PhotoZoom'
 import { images } from 'ionicons/icons'
 import { useForm } from '../utils/Hooks'
+import {InputControls, InputTextArea, UnitTypeInput} from '../components/Input'
+import {lockOpen,fileTrayStacked,pricetag,removeCircle,text,bagAdd} from 'ionicons/icons'
 
 function CommodityCreate(props) {
     const [UploadProgress, setUploadProgress] = useState(false)
     const [selectedImage, setSelectedImage] = useState(null)
     const [uploadedImages, setUploadedImages] = useState([])
     const [errors, setError] = useState({})
+
+    const [unit_type, setUnit_type] = useState()
+    const selectCalcUnitHandler = (value) => {
+        setUnit_type(value)
+    }
 
     const { onChange, onSubmit, values } = useForm(loginUser, {
         name: '',
@@ -25,6 +32,13 @@ function CommodityCreate(props) {
 
     const clearError = () => {
         setError({})
+    }
+
+
+    //clean inputed data when user click back
+    function EmptyImageContainer(){
+        props.stateHandler(false)
+        setUploadedImages([])
     }
 
     //upload section
@@ -74,24 +88,53 @@ function CommodityCreate(props) {
                 <IonHeader translucent>
                     <IonToolbar color='warning'>
                         <IonButtons slot="start">
-                            <IonBackButton defaultHref="/Posts" onClick={() => props.stateHandler(false)} />
+                            <IonBackButton defaultHref="/Posts" onClick={() => EmptyImageContainer()} />
                         </IonButtons>
                         <IonTitle>Post new Commodity!</IonTitle>
                     </IonToolbar>
                 </IonHeader>
                 <IonContent scrollEvents={true}>
-                    <IonItemGroup>
-                        {uploadedImages.length > 0 && uploadedImages.map((image, i) => (
-                            <IonItem key={i}>
-                                <IonAvatar className="ion-avatar">
+                    <InputControls 
+                            type="text" 
+                            display={ <div><IonIcon icon={fileTrayStacked}/> Commodity Name</div>}
+                            name="name" 
+                            onChange={onChange} 
+                            value={values.name}/>
+                    <InputControls 
+                            type="number" 
+                            display={ <div><IonIcon icon={pricetag}/> Unit Price</div>}
+                            name="unit_price" 
+                            onChange={onChange} 
+                            value={values.unit_price}/>
+                    <InputControls 
+                            type="number" 
+                            display={ <div><IonIcon icon={removeCircle}/> Minimum Purchase</div>}
+                            name="min_purchase" 
+                            onChange={onChange} 
+                            value={values.min_purchase}/>
+                    <InputTextArea 
+                            display={ <div><IonIcon icon={text}/> Commodity Description</div>}
+                            name="description" 
+                            onChange={onChange} 
+                            value={values.description}
+                    />
+                    <UnitTypeInput unit_type={unit_type} onSelectValue={selectCalcUnitHandler} name="unit_type" onChange={onChange} value={values.unit_type}/> 
+                    <IonItemGroup className="attachment-group">                 
+                        {uploadedImages.length > 0 && uploadedImages.map((image, i) => (                        
+                                <IonAvatar className="attachment ion-avatar" key={i}>
                                     <ImageZoom src={image} height="70px" width="70px" />
-                                </IonAvatar>
-                            </IonItem>
-                        ))}
+                                </IonAvatar>                         
+                        ))}                              
                     </IonItemGroup>
-                    <IonFab vertical="top" horizontal="end" slot="fixed">
-                        <IonFabButton color="warning">{!UploadProgress ? <IonIcon icon={images} className="gallery"></IonIcon> : <IonSpinner className="gallery" color="dark" />}<input type="file" onChange={fileSelecterHandler} /></IonFabButton>
-                    </IonFab>
+                    <IonButton                    
+                        expand="block"  
+                        color="warning"                     
+                        className="create-post-button" 
+                        router-direction="forward" routerAnimation 
+                        onIonFocus={onSubmit}><IonIcon slot="start" icon={bagAdd} />Create Post</IonButton>
+                    <IonFab vertical="bottom" horizontal="end" slot="fixed">
+                            <IonFabButton color="warning" disabled={uploadedImages.length === 8 ? true : false}>{!UploadProgress ? <IonIcon icon={images} className="gallery"></IonIcon> : <IonSpinner className="gallery" color="dark" />}<input type="file" onChange={fileSelecterHandler} /></IonFabButton>
+                        </IonFab>
                 </IonContent>
             </IonModal>
         </React.Fragment>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {IonItem,IonLabel,IonInput,IonSelect,IonSelectOption, IonIcon} from '@ionic/react'
-import {ticket} from 'ionicons/icons'
+import {IonItem,IonLabel,IonInput,IonSelect,IonSelectOption, IonIcon, IonTextarea} from '@ionic/react'
+import {ticket,clipboard} from 'ionicons/icons'
 
 
 function InputControls(props){
@@ -9,6 +9,16 @@ function InputControls(props){
         <IonItem>
             <IonLabel position="floating" color="medium">{props.errorMessage ? props.errorMessage : props.display}</IonLabel>
             <IonInput type={props.type} ref={props.ref} name={props.name} autofocus={props.focus} clearInput="true" onIonChange={props.onChange} ></IonInput>
+        </IonItem>
+    )
+}
+
+function InputTextArea(props){
+
+    return(
+        <IonItem>
+            <IonLabel position="floating" color="medium">{props.errorMessage ? props.errorMessage : props.display}</IonLabel>
+            <IonTextarea  ref={props.ref} name={props.name} autofocus={props.focus} clearInput="true" onIonChange={props.onChange} ></IonTextarea>
         </IonItem>
     )
 }
@@ -33,4 +43,22 @@ function RoleInput(props){
     )
 }
 
-export {InputControls,RoleInput}
+function UnitTypeInput(props){
+    const inputChangeHandler = (event) => {
+        props.onSelectValue(event.detail.value)
+        props.onChange(event)
+    }
+
+    return(
+        <IonItem className="ion-margin-top">
+            <IonLabel><IonIcon icon={clipboard} /> Unit Type</IonLabel>
+            <IonSelect value={props.unit_type} name={props.name} placeholder={props.placeholder ? props.placeholder : "Unit Type"} onIonChange={inputChangeHandler} okText="Okay" cancelText="Dismiss">
+              <IonSelectOption value="Kg">Kg</IonSelectOption>
+              <IonSelectOption value="Unit">Unit</IonSelectOption>
+            </IonSelect>
+        </IonItem>
+        
+    )
+}
+
+export {InputControls,RoleInput,UnitTypeInput,InputTextArea}
